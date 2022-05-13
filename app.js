469905075f50ff08ddb9adcf4580f6f6950f0297
Mario Cimmino <mariocimm@gmail.com>
2022-05-12 20:09:02 -0600

    Add global COP API functionality

---
+++ app.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", ()=>{
+    pullMainCOP();
+});
+
+async function pullMainCOP (){
+
+    const price = document.querySelector('.current-currency-price')
+    price.innerHTML = 'loading';
+
+    // Grab Data From API
+    const universalCOP = await fetch('https://v6.exchangerate-api.com/v6/a138cb23d3d427fff2d4bdad/latest/USD')
+        .then((response) => response.json())
+        .then ((json) => (json.conversion_rates.COP));
+    
+    price.innerHTML = universalCOP.toFixed(2);
+};
+
+
+
+

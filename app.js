7ef5dfdd0eadfa8e122191b4c4240c385603dbb8
Mario Cimmino <mariocimm@gmail.com>
2022-06-01 22:17:20 -0600

    Updated pullCOP function to use templates. Move HTML into JS

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
+let container, template
 
 const sources = [
     {
-        className: 'global',
         url : 'https://v6.exchangerate-api.com/v6/a138cb23d3d427fff2d4bdad/latest/USD',
         getJsonRate : (json) => (json.conversion_rates.COP)
     },
     {
-        className: 'wise',
         url : 'https://api.transferwise.com/v1/rates?source=USD&target=COP',
         fetchOptions : {headers: {'Authorization': 'Bearer d55a6108-bb23-4e8e-a4f8-3cc340331f08'}},
+        image : 'wise-logo.png',
         getJsonRate : (json) => (json[0].rate)
     },
+    {
+        image: 'visa.png',
+    },
+    {
+        image: 'revolut.png',
+    },
+    {
+        image: 'xoom-logo.png',
+    },
+    {
+        image: 'WorldRemit-logo.png',
+    },
+    {
+        image: 'remitly.png',
+    },
+    {
+        image: 'xe-money-transfer-logo.png',
+    },
+    {
+        image: 'small-world-money-transfer-blue.png',
+    },
+    {
+        image: 'paysend-logo.png',
+    },
+
+
 ]
 
 
 window.addEventListener("load", ()=>{
     const reloadBtn = document.querySelector('.reload-btn')
     reloadBtn.addEventListener('click', pull)
+    container = document.querySelector('.sources-container')
+    template = document.querySelector('.conversion-source-cell')
     pull();
 });
 
@@ -24,20 +52,31 @@ window.addEventListener("load", ()=>{
 async function pull (){
     document.body.classList.add('loading')
 
+    container.innerHTML = '';
+
     sources.forEach(source => pullCOP(source))
 
     document.body.classList.remove('loading')
 }
 
-async function pullCOP (options){
-
-    const price = document.querySelector(`.current-${options.className}-cop-price`)
-    price.innerHTML = 'loading';
+async function pullCOP (source){
 
     // Grab Data From API
-    const rate = await fetch(options.url, options.fetchOptions)
+
+    const element = template.cloneNode(true)
+    container.appendChild(element);
+
+    const image = element.querySelector('img')
+    image.setAttribute('src', `/images/${source.image}`);
+
+    let rate = 0;
+
+    if (source.url){
+        rate = await fetch(source.url, source.fetchOptions)
         .then((response) => response.json())
-        .then ((json) => options.getJsonRate(json));
-    
+        .then ((json) => source.getJsonRate(json));
+    } 
+
+    const price = element.querySelector('.current-cop-price')
     price.innerHTML = `$ ${rate.toFixed(2)} COP`;
 };

dd3cdf6b846e0ad85219a2631e1825f63b7e236d
Mario Cimmino <mariocimm@gmail.com>
2022-06-02 20:43:29 -0600

    Update promises to wait for GlobalCOP

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let container, template
 
-const sources = [
-    {
+const globalCOP = {
         url : 'https://v6.exchangerate-api.com/v6/a138cb23d3d427fff2d4bdad/latest/USD',
         getJsonRate : (json) => (json.conversion_rates.COP)
-    },
+}
+
+const sources = [
     {
         url : 'https://api.transferwise.com/v1/rates?source=USD&target=COP',
         fetchOptions : {headers: {'Authorization': 'Bearer d55a6108-bb23-4e8e-a4f8-3cc340331f08'}},
@@ -43,10 +44,15 @@ const sources = [
 window.addEventListener("load", ()=>{
     const reloadBtn = document.querySelector('.reload-btn')
     reloadBtn.addEventListener('click', pull)
+
     container = document.querySelector('.sources-container')
     template = document.querySelector('.conversion-source-cell')
     container.innerHTML = '';
+
     sources.map(source => createSourceCell(source));
+
+    globalCOP.price = document.querySelector('.current-global-cop-price');
+
     pull();
 });
 
@@ -54,7 +60,10 @@ window.addEventListener("load", ()=>{
 async function pull (){
     document.body.classList.add('loading')
 
+    const globalCOPPromise = pullCOP(globalCOP);
+
     const promises = sources.map(source => pullCOP(source))
+    promises.push(globalCOPPromise);
     await Promise.all(promises)
 
     document.body.classList.remove('loading')
